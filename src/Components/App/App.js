146581ff4ca3838a42f8faaf0465619810cfd7f7
Jason Chan <jchan321738@gmail.com>
2021-10-27 14:44:28 -0700

    add tracks to playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -21,6 +21,17 @@ class App extends React.Component {
         {playlistName: 'plname3', playlistArtist: 'plartist3', playlistAlbum: 'plalbum3',  id: 6}
       ]
     }
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return
+    }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
   }
 
   render() {
@@ -30,7 +41,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist 
               playlistName={this.state.playlistName} 
               playlistTracks={this.state.playlistTracks} 
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+    constructor(props) {
+        super(props);
+        this.addTrack = this.addTrack.bind(this);
+    }
+
     renderAction() {
         if (this.props.isRemoval) {
             <button className="Track-action">-</button>
         } else {
-            <button className="Track-action">+</button>
+            <button className="Track-action" onClick={this.addTrack}>+</button>
         }
+    }
+
+    addTrack() {
+        this.props.addTrack(this.props.track)
     }
 
     render() {
