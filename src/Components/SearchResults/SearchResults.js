5766bdd15c8041ee14fc4201e6cda90fa786e5e6
Jason Chan <jchan321738@gmail.com>
2021-10-27 13:46:42 -0700

    pass down searchResult, render resultList

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './SearchResults.css';
 
 class SearchResults extends React.Component {
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                <TrackList />
+                <TrackList tracks={this.props.searchResults} />
             </div>
         )
     }
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Track.css';
 
 class Track extends React.Component {
     renderAction() {
@@ -13,8 +14,8 @@ class Track extends React.Component {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3><!-- track name will go here --></h3>
-                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
                 <button className="Track-action"></button>
             </div>
